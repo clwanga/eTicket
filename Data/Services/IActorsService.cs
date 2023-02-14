69a2784eb50f537a65925119b521b417ee960e5f
Charles Maungila <clwanga1095@gmail.com>
2023-02-14 15:49:56 +0300

    Designed the navigation bar

---
+++ Data/Services/IActorsService.cs
@@ -0,0 +1,18 @@
+﻿using eTicket.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTicket.Data.Services
+{
+    public interface IActorsService
+    {
+        IEnumerable<Actors> GetAll();
+        Actors GetById(int id);
+        void Add(Actors actor);
+        Actors Update(int id, Actors newActor);
+        void Delete(int id);
+    }
+    
+}
